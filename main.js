5695db6ac267b25cb353260922ec224a3debb750
matheuspenas <mathegupena@gmail.com>
2025-03-14 20:29:31 -0300

    adição do java script

---
+++ main.js
@@ -0,0 +1,6 @@
+// simulação de adicão de produtos ao carrinho
+
+const botoesAdicionar = document.querySelectorAll('.adicionar');
+const listaPedido     = document.getElementById('.listapedidos');
+const totalElemento   = document.getElementById('total');
+let   total           = 0;   
